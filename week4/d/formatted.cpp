ff42cc0810ebd35882908a7af4034e558a44bdad
Александр Абрамов <ttpo100ajiex@mail.ru>
2022-10-18 17:07:06 +0300

    HW4

--- week4/d/formatted.cpp
+++ week4/d/formatted.cpp
@@ -1,60 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <iterator>
-#include <climits>
+
+int findDoubledMedian(int* a, int* b, unsigned int size) {
+    if (size == 2) {
+        return (a[0] + a[1] + b[0] + b[1] - std::min(std::min(a[0], a[1]), std::min(b[0], b[1])) -
+                std::max(std::max(a[0], a[1]), std::max(b[0], b[1])));
+    }
+    int part_medians = (size - 1) / 2;
+    if (a[part_medians] < b[part_medians]) {
+        return findDoubledMedian(a + part_medians, b, size - part_medians);
+    } else {
+        return findDoubledMedian(a, b + part_medians, size - part_medians);
+    }
+}
 
 int main(void) {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    int n, m;
+    unsigned int n, m;
     std::cin >> n >> m;
-    std::vector<std::vector<int> > data(n, std::vector<int>(m + 2));
-    for (int i = 0; i < n; i++) {
-        for (int j = 1; j <= m; j++) {
+    std::vector<std::vector<int> > data(n, std::vector<int>(m));
+    for (unsigned int i = 0; i < n; i++) {
+        for (unsigned int j = 0; j < m; j++) {
             std::cin >> data[i][j];
         }
-        data[i][0] = INT_MIN;
-        data[i][m + 1] = INT_MAX;
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            int left = 0, right = m + 1;
-            while (right - left > 1) {
-                int middle = (left + right) / 2;
-                std::vector<int>::iterator first_higher =
-                    std::lower_bound(data[j].begin(), data[j].end(), data[i][middle]);
-                int dist = middle + std::distance(data[j].begin(), first_higher) - 1;
-                if (dist >= m) {
-                    right = middle;
-                } else {
-                    left = middle;
-                }
-            }
-            std::vector<int>::iterator first_higher =
-                std::lower_bound(data[j].begin(), data[j].end(), data[i][left]);
-            int dist = left + std::distance(data[j].begin(), first_higher) - 1;
-            int last_elem = data[i][left] == INT_MIN ? *(first_higher - 1) : data[i][left];
-            left++;
-            while (dist != m) {
-                if (*first_higher < data[i][left]) {
-                    last_elem = *(first_higher++);
-                } else {
-                    last_elem = data[i][left++];
-                }
-                dist++;
-            }
-            int sum = last_elem + std::min(*first_higher, data[i][left]);
-            if (sum < 0) {
+    for (unsigned int i = 0; i < n; i++) {
+        for (unsigned int j = i + 1; j < n; j++) {
+            int median = findDoubledMedian(data[i].data(), data[j].data(), m);
+            if (median < 0) {
                 std::cout << "-";
-                sum = -sum;
+                median = -median;
             }
-            if (sum % 2 == 1) {
-                std::cout << sum / 2 << ".50000\n";
+            if (median % 2 == 1) {
+                std::cout << median / 2 << ".50000\n";
             } else {
-                std::cout << sum / 2 << ".00000\n";
+                std::cout << median / 2 << ".00000\n";
             }
         }
     }
--- week4/d/index.cpp
+++ week4/d/index.cpp
@@ -1,50 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <iterator>
-#include <climits>
+
+int find_doubled_median(int* A, int* B, unsigned int size)
+{
+    if (size == 2) return(A[0] + A[1] + B[0] + B[1] - std::min(std::min(A[0], A[1]), std::min(B[0], B[1])) - std::max(std::max(A[0], A[1]), std::max(B[0], B[1])));
+    int part_medians = (size - 1) / 2;
+    if (A[part_medians] < B[part_medians]) return find_doubled_median(A + part_medians, B, size - part_medians);
+    else return find_doubled_median(A, B + part_medians, size - part_medians);
+}
 
 int main(void)
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    int n, m;
+    unsigned int n, m;
     std::cin >> n >> m;
-    std::vector< std::vector<int> > data(n, std::vector<int>(m + 2));
-    for (int i = 0; i < n; i++)
+    std::vector< std::vector<int> > data(n, std::vector<int>(m));
+    for (unsigned int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= m; j++) std::cin >> data[i][j];
-        data[i][0] = INT_MIN; data[i][m + 1] = INT_MAX;
+        for (unsigned int j = 0; j < m; j++) std::cin >> data[i][j];
     }
 
-    for (int i = 0; i < n; i++)
+    for (unsigned int i = 0; i < n; i++)
     {
-        for (int j = i + 1; j < n; j++)
+        for (unsigned int j = i + 1; j < n; j++)
         {
-            int left = 0, right = m + 1;
-            while (right - left > 1)
-            {
-                int middle = (left + right) / 2;
-                std::vector<int>::iterator first_higher = std::lower_bound(data[j].begin(), data[j].end(), data[i][middle]);
-                int dist = middle + std::distance(data[j].begin(), first_higher) - 1;
-                if (dist >= m) right = middle;
-                else left = middle;
-            }
-            std::vector<int>::iterator first_higher = std::lower_bound(data[j].begin(), data[j].end(), data[i][left]);
-            int dist = left + std::distance(data[j].begin(), first_higher) - 1;
-            int last_elem = data[i][left] == INT_MIN ? *(first_higher - 1) : data[i][left];
-            left++;
-            while (dist != m)
-            {
-                if (*first_higher < data[i][left]) last_elem = *(first_higher++);
-                else last_elem = data[i][left++];
-                dist++;
-            }
-            int sum = last_elem + std::min(*first_higher, data[i][left]);
-            if (sum < 0) { std::cout << "-"; sum = -sum; }
-            if (sum % 2 == 1) std::cout << sum / 2 << ".50000\n";
-            else std::cout << sum / 2 << ".00000\n";
+            int median = find_doubled_median(data[i].data(), data[j].data(), m);
+            if (median < 0) { std::cout << "-"; median = -median; }
+            if (median % 2 == 1) std::cout << median / 2 << ".50000\n";
+            else std::cout << median / 2 << ".00000\n";
         }
     }
 
