fc4398269b4dee317016d54f8bb9934c53da151d
Александр Абрамов <ttpo100ajiex@mail.ru>
2023-01-20 20:50:35 +0300

    Week 2-2 Initial

---
+++ week2-2/e/index.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ios>
+#include <vector>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    unsigned int n; std::cin >> n;
+    std::vector <int> keys(n);
+    for (unsigned int i = 0; i < n; i++) std::cin >> keys[i];
+    unsigned int k; std::cin >> k;
+    for (unsigned int i = 0; i < k; i++)
+    {
+        unsigned int index;
+        std::cin >> index;
+        keys[index - 1]--;
+    }
+    for (unsigned int i = 0; i < n; i++) std::cout << (keys[i] >= 0 ? "no" : "yes") << "\n";
+    return 0;
+}
