415c3d144abfea88f59f476cf02caa6a3b1232e0
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2023-03-21 13:59:23 +0300

    Removed leak sanitizer

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ cp $solution_file solution_save.cpp
 clang-format -i $solution_file
 clang-tidy $solution_file -fix-errors --
 clang-format -i $solution_file
-g++ $index_file -fsanitize=address,undefined,leak -fno-sanitize-recover=all -std=c++17 -O2 -Wall -Werror -Wsign-compare -o solution.exe
+g++ $index_file -fsanitize=address,undefined -fno-sanitize-recover=all -std=c++17 -O2 -Wall -Werror -Wsign-compare -o solution.exe
 
 mv $solution_file $formatted_file
 cp solution_save.cpp $solution_file
