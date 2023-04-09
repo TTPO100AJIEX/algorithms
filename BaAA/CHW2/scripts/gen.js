bb7ae2320da708e8f4767215a3e4915d39df99f3
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2023-04-09 16:01:31 +0300

    Comments

--- BaAA/CHW2/scripts/gen.js
+++ BaAA/CHW2/scripts/gen.js
@@ -38,10 +38,7 @@ function generateTest(text, pattern, substitutionSymbolsAmount)
     pattern = pattern.split('');
     for (let i = 0; i < substitutionSymbolsAmount; i++)
     {
-        const partSize = Math.floor(pattern.length / (substitutionSymbolsAmount + 1));
-        const index = partSize * (i + 1) + Math.round((Math.random() - 0.5) * partSize * 0.5);
-
-        // const index = Math.floor(Math.random() * pattern.length);
+        const index = Math.floor(Math.random() * pattern.length);
         if (pattern[index] == '?') i--;
         pattern[index] = '?';
     }
