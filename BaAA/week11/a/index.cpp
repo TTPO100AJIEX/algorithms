5719c9293deb991478ebf14db522af8073e7d887
Александр Абрамов <ttpo100ajiex@mail.ru>
2023-04-24 00:13:06 +0300

    Week-11 Dump

--- BaAA/week11/a/index.cpp
+++ BaAA/week11/a/index.cpp
@@ -1,16 +1,9 @@
 #include <ios>
 #include <iostream>
 #include <vector>
-#include <set>
 #include <algorithm>
-
-enum class VisitedStatus : unsigned char { FALSE, VISITED, VISITING };
-void try_to_visit(unsigned int current, std::vector<VisitedStatus>& visited, const std::vector< std::set<unsigned int> >& edges)
-{
-    if (visited[current] != VisitedStatus::FALSE) return;
-    visited[current] = VisitedStatus::VISITING;
-    for (unsigned int sibling : edges[current]) try_to_visit(sibling, visited, edges);
-}
+#include <queue>
+#include <utility>
 
 int main()
 {
@@ -18,34 +11,39 @@ int main()
     std::cin.tie(nullptr);
 
     unsigned int n, k; std::cin >> n >> k;
-    std::vector< std::set<unsigned int> > edges(n);
-    std::vector<VisitedStatus> visited(n, VisitedStatus::FALSE); visited[0] = VisitedStatus::VISITED;
+    std::vector< std::vector< std::pair<unsigned int, unsigned int> > > edges(n);
     for (unsigned int i = 0; i < k; i++)
     {
-        for (unsigned int i = 0; i < n; i++) edges[i].clear();
         unsigned int amount; std::cin >> amount;
         for (unsigned int j = 0; j < amount; j++)
         {
             unsigned int from, to;
             std::cin >> from >> to;
             from--; to--;
-            edges[from].insert(to);
-            edges[to].insert(from);
-        }
-        for (unsigned int i = 0; i < n; i++)
-        {
-            if (visited[i] == VisitedStatus::VISITED)
-            {
-                visited[i] = VisitedStatus::FALSE;
-                try_to_visit(i, visited, edges);
-            }
-        }
-        if (visited[n - 1] == VisitedStatus::VISITING) { std::cout << i + 1; return 0; }
-        for (unsigned int i = 0; i < n; i++)
-        {
-            if (visited[i] == VisitedStatus::VISITING) visited[i] = VisitedStatus::VISITED;
+            edges[from].emplace_back(to, i + 1);
+            edges[to].emplace_back(from, i + 1);
         }
     }
-    std::cout << -1;
+
+
+    constexpr unsigned int distanceMax = (1 << 30);
+    std::vector<unsigned int> vertexes(n, distanceMax); vertexes[0] = 0;
+    std::queue< std::pair<unsigned int, unsigned int> > to_update; to_update.emplace(0, distanceMax);
+    while (!to_update.empty())
+    {
+        std::pair<unsigned int, unsigned int> current = to_update.front();
+        to_update.pop();
+        for (const std::pair<unsigned int, unsigned int>& edge : edges[current.first])
+        {
+            unsigned int newDistance = vertexes[current.first] + (current.second == edge.second ? 0 : 1);
+            if (vertexes[edge.first] >= newDistance)
+            {
+                vertexes[edge.first] = newDistance;
+                to_update.emplace(edge.first, edge.second);
+            }
+        }
+    }
+
+    std::cout << (vertexes[n - 1] == distanceMax) ? -1 : vertexes[n - 1];
     return 0;
 }
---
+++ BaAA/week11/d/formatted.cpp
@@ -0,0 +1,58 @@
+#include <ios>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n, x1, y1, x2, y2;
+    std::cin >> n >> x1 >> y1 >> x2 >> y2;
+
+    using Vertex = std::pair<int, int>;
+    constexpr unsigned int kDistanceMax = (1 << 30);
+
+    std::vector<std::vector<unsigned int> > distance(n, std::vector<unsigned int>(n, kDistanceMax));
+    std::vector<std::vector<Vertex> > previous(n, std::vector<Vertex>(n));
+    --x1;
+    --y1;
+    --x2;
+    --y2;
+    distance[x1][y1] = 0;
+    previous[x1][y1] = {-1, -1};
+
+    std::queue<Vertex> to_update;
+    to_update.emplace(x1, y1);
+    while (distance[x2][y2] == kDistanceMax) {
+        Vertex current = to_update.front();
+        to_update.pop();
+
+        Vertex siblings[8] = {
+            {current.first + 2, current.second + 1}, {current.first + 2, current.second - 1},
+            {current.first - 2, current.second + 1}, {current.first - 2, current.second - 1},
+            {current.first + 1, current.second + 2}, {current.first + 1, current.second - 2},
+            {current.first - 1, current.second + 2}, {current.first - 1, current.second - 2}};
+        for (const Vertex& sibling : siblings) {
+            if (sibling.first < 0 || sibling.second < 0 || sibling.first >= n ||
+                sibling.second >= n || distance[sibling.first][sibling.second] != kDistanceMax) {
+                continue;
+            }
+            to_update.push(sibling);
+            distance[sibling.first][sibling.second] = distance[current.first][current.second] + 1;
+            previous[sibling.first][sibling.second] = current;
+        }
+    }
+
+    std::cout << distance[x2][y2] << '\n';
+    Vertex current = {x2, y2};
+    std::vector<Vertex> path;
+    while (current.first >= 0) {
+        path.push_back(current);
+        current = previous[current.first][current.second];
+    }
+    for (std::vector<Vertex>::const_reverse_iterator iter = path.rbegin(); iter != path.rend();
+         ++iter) {
+        std::cout << iter->first + 1 << ' ' << iter->second + 1 << '\n';
+    }
+}
